a6c3e97c7a462f152884fe16d79177afffeed888
pangolongomo <pangolongomo@gmail.com>
2023-12-04 21:30:43 +0100

    change header star icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-
+import { AutoAwesomeOutlined } from "@mui/icons-material";
 
 function Header() {
   return (
     <header className="header">
       <h1 className="page-title">Home</h1>
-      <img src="https://res.cloudinary.com/dvmqqgrx5/image/upload/v1701449371/Project-07-Twitter-Clone-1-GhislainP/icons/stars_synvep.svg" alt="stars" className="top-tweets" />
+      <AutoAwesomeOutlined fontSize="large" />
     </header>
   );
 }
